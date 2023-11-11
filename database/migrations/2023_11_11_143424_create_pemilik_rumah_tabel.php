90c5d4070f95cc6f4d9c27b5facfef074ff405be
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-11 21:37:23 +0700

    edit migration of pemilik rumah - add rumah_id column

--- database/migrations/2023_11_11_143424_create_pemilik_rumah_tabel.php
+++ database/migrations/2023_11_11_143424_create_pemilik_rumah_tabel.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pemilik_rumah_tabel', function (Blueprint $table) {
             $table->id();
             $table->string("nama_lengkap");
+            $table->int("rumah_id");
             $table->foreign("rumah_id")->references("id")->on("perumahan");
             $table->timestamps();
         });
