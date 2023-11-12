8a21940188c9ee1ae5fb38c78a07f52b20249a62
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-12 23:53:13 +0700

    Adjust the form field and add field year and month

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -15,6 +15,29 @@
         </div>
 
         <div class="mb-3">
+            <label for="year" class="form-label">Year</label>
+            <input type="text" class="form-control" id="year" name="year" required>
+        </div>
+        
+        <div class="mb-3">
+            <label for="month" class="form-label">Month</label>
+            <select class="form-control" id="month" name="month" required>
+                <option value="Januari">Januari</option>
+                <option value="Februari">Februari</option>
+                <option value="Maret">Maret</option>
+                <option value="April">April</option>
+                <option value="Mei">Mei</option>
+                <option value="Juni">Juni</option>
+                <option value="Juli">Juli</option>
+                <option value="Agustus">Agustus</option>
+                <option value="September">September</option>
+                <option value="Oktober">Oktober</option>
+                <option value="November">November</option>
+                <option value="Desember">Desember</option>
+            </select>
+        </div>
+
+        <div class="mb-3">
             <label for="is_paid" class="form-label">Is Paid</label>
             <select class="form-control" id="is_paid" name="is_paid">
                 <option value="0">No</option>
