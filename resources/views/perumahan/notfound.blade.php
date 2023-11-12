e5c28eeef5b3c0bf9fb6bf8e7abdbb9fb05490bb
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-12 14:28:46 +0700

    generate and filled up basic table for updating and read all

---
+++ resources/views/perumahan/notfound.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perumahan Not Found</title>
+</head>
+<body>
+    <h1>Perumahan Not Found</h1>
+    <p>The requested Perumahan record was not found.</p>
+
+    <a href="{{ url('/perumahan') }}">Back to Home</a>
+</body>
+</html>
