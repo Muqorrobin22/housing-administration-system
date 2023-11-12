08053cbe773e1de69faf79ae8ceef86260f34ee2
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-12 16:10:26 +0700

    create route for pemilik rumah (getAll, create form, and create functionality)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PemilikRumahController;
 use App\Http\Controllers\PerumahanController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Perumahan
 Route::get("/perumahan", [PerumahanController::class, "index"])->name("perumahan.home");
-
 Route::get('/perumahan/{id}', [PerumahanController::class, 'show'])->name("perumahan.show");
-
-// Update Route
 Route::put('/perumahan/{id}', [PerumahanController::class, 'update'])->name("perumahan.update");
+
+// PemilikRumah
+Route::get("/pemilik_rumah", [PemilikRumahController::class, "index"])->name("pemilik_rumah.index");
+Route::get("/pemilik_rumah/create", [PemilikRumahController::class, "create"])->name("pemilik_rumah.create");
+Route::post("/pemilik_rumah", [PemilikRumahController::class, "store"])->name("pemilik_rumah.store");
+
+
