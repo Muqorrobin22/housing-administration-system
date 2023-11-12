5000afa2d95e5ddcb5ded65f99a6ccff0e52b2d8
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-12 14:29:16 +0700

    Update the Model Perumahan

--- app/Models/Perumahan.php
+++ app/Models/Perumahan.php
@@ -10,18 +10,7 @@ class Perumahan extends Model
     use HasFactory;
 
     protected $table = "perumahan";
+    protected $guarded = [];
+    // protected $fillable = ['nama_rumah', 'no_rumah', 'is_occupied'];
 
-    public function findPerumahan($id)
-    {
-
-        $perumahan = Perumahan::find($id);
-
-        if(!$perumahan) {
-            return response()->json(["message" => "Perumahan tidak ditemukan", 404]);
-        }
-
-        return response()->json($perumahan);
-
-    }
- 
 }
