0640b88abec6a1712ccb5568863c2658df18b27d
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-11-12 23:57:04 +0700

    Adjust the form field and add field year and month

--- resources/views/payments/edit.blade.php
+++ resources/views/payments/edit.blade.php
@@ -16,6 +16,29 @@
         </div>
 
         <div class="mb-3">
+            <label for="year" class="form-label">Year</label>
+            <input type="text" class="form-control" id="year" name="year" maxlength="4" value="{{ $payment->year }}" required>
+        </div>
+        
+        <div class="mb-3">
+            <label for="month" class="form-label">Month</label>
+            <select class="form-control" id="month" name="month" required>
+                <option value="Januari" {{ $payment->month == 'Januari' ? 'selected' : '' }}>Januari</option>
+                <option value="Februari" {{ $payment->month == 'Februari' ? 'selected' : '' }}>Februari</option>
+                <option value="Maret" {{ $payment->month == 'Maret' ? 'selected' : '' }}>Maret</option>
+                <option value="April" {{ $payment->month == 'April' ? 'selected' : '' }}>April</option>
+                <option value="Mei" {{ $payment->month == 'Mei' ? 'selected' : '' }}>Mei</option>
+                <option value="Juni" {{ $payment->month == 'Juni' ? 'selected' : '' }}>Juni</option>
+                <option value="Juli" {{ $payment->month == 'Juli' ? 'selected' : '' }}>Juli</option>
+                <option value="Agustus" {{ $payment->month == 'Agustus' ? 'selected' : '' }}>Agustus</option>
+                <option value="September" {{ $payment->month == 'September' ? 'selected' : '' }}>September</option>
+                <option value="Oktober" {{ $payment->month == 'Oktober' ? 'selected' : '' }}>Oktober</option>
+                <option value="November" {{ $payment->month == 'November' ? 'selected' : '' }}>November</option>
+                <option value="Desember" {{ $payment->month == 'Desember' ? 'selected' : '' }}>Desember</option>
+            </select>
+        </div>
+
+        <div class="mb-3">
             <label for="is_paid" class="form-label">Is Paid</label>
             <select class="form-control" id="is_paid" name="is_paid">
                 <option value="0" {{ !$payment->is_paid ? 'selected' : '' }}>No</option>
